f58a96683ce8eb1a8d0e89d3112d801585942de0
Håvard Vidme <havard.vidme@gmail.com>
2016-11-15 10:24:34 +0100

    Added JSX and script functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 
@@ -11,6 +13,25 @@ gulp.task('pug:build', function () {
   .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('default', ['pug:build'], function () {
+gulp.task('jsx:build', function () {
+  return gulp.src('./src/jsx/*.jsx')
+  .pipe(babel({
+    presets: ['react']
+  }))
+  .pipe(concat('statusmonitor.js'))
+  .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('vendorJs:build', function () {
+  return gulp.src([
+    './bower_components/react/react.js',
+    './bower_components/react/react-dom.js'
+  ])
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('default', ['pug:build', 'vendorJs:build', 'jsx:build'], function () {
   gulp.watch('./src/pug/**/*.pug', ['pug:build']);
+  gulp.watch('./src/jsx/**/*.jsx', ['jsx:build']);
 });
---
+++ src/jsx/StatusMonitor.jsx
@@ -0,0 +1,10 @@
+/**
+ * StatusMonitor
+ */
+var StatusMonitor = React.createClass({
+  render: function () {
+    return (
+      <div>Test</div>
+    );
+  }
+});
