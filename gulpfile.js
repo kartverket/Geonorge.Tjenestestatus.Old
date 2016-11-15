28c4ed33396d858a6faedfcb5e627e6b9bf23039
Håvard Vidme <havard.vidme@gmail.com>
2016-11-15 10:13:11 +0100

    Added Pug for HTML templating

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
+var pug = require('gulp-pug');
 
-gulp.task('default', [], function () {
+gulp.task('pug:build', function () {
+  return gulp.src('./src/pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./build/'));
+});
 
+gulp.task('default', ['pug:build'], function () {
+  gulp.watch('./src/pug/**/*.pug', ['pug:build']);
 });
